7ac13104b4718e9f5082d28404e03530136bbcb8
Vipul-045 <vipulyadav0479@gmail.com>
2024-01-28 09:32:29 +0530

    Cretaed Routes and Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/about'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 
 export default function App() {
-  return (
-    <div>App</div>
+  return(
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/> } />
+        <Route path='/about' element={<About />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/Project' element={<Projects />} />
+        <Route path='/Signin' element={<Signin />} />
+        <Route path='/Signup' element={<Signup />} />
+      </Routes>
+      </BrowserRouter>
   )
 }
